54bf55999969e509a09921c5b1950f28c3553841
Helder <helder@pop-os.localdomain>
2022-04-01 00:26:16 -0400

    MyCommit

---
+++ RDS/rds.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  db_name              = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
